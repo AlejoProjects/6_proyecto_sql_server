cba29cb6dcc3aa9d065d8b1620199797531b132b
AlejoProjects <alejodelavinci@gmail.com>
2023-09-02 16:19:39 -0500

    migrations updated

--- migrations/20230816025128_cliente.js
+++ migrations/20230816025128_cliente.js
@@ -6,8 +6,14 @@ exports.up = function(knex) {
     if(!exists){
   return knex.schema.hasTable('cliente').then((exists)=> {
         table.increments("dni").primary();
-        table.string("nombre").notNUllable();
+        table.string("name").notNUllable();
         table.string("primer_apellido").notNUllable();
+        table.string("name").notNUllable();
+        table.string("primer_apellido").notNUllable();
+        table.string("telefono").notNUllable(); 
+        table.string("email").notNUllable();
+        table.string("codigo_postal").notNUllable();
+        table.string("barrio").notNUllable();
         table.boolean("active").notNUllable().defaultTo(true);
         table.timestamp("created_at").defaultTo(knex.fn.now());
   });
--- migrations/20230816025130_producto.js
+++ migrations/20230816025130_producto.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 exports.up = function(knex) {
-  
+    return knex.schema.hasTable('producto').then((exists) => {
+        if (!exists) {
+            return knex.schema.createTable("producto", function (table) {
+              table.string("sku").primary();
+              table.string("name").notNullable();
+              table.string("description").notNullable();
+              table.float("price").notNullable();
+            });
+          }
+    });
 };
 
 /**
