49bb1fb4011df97d6c29a288db3086be1d07136f
AlejoProjects <alejodelavinci@gmail.com>
2023-09-02 17:09:15 -0500

    Multiple products solved

--- routes/SaleRoutes.js
+++ routes/SaleRoutes.js
@@ -1,22 +1,20 @@
 const express = require('express');
+const { SaleController } = require('../controllers');
 
 const router = express.Router();
 
-const { SaleController } = require('../controllers');
+
 
 
 //crear
 router.post('/', SaleController.createSale);
 //leer
-router.get('/', );
-router.get('/:id',);
+
 //actualizar
 router.put('/:id', (req,res) => res.send({message: " Se sustituyp"}));
+
 router.patch('/:id', (req,res) => res.send({message: " Se modificó"}));
 
 //delete
 router.delete('/', (req,res) => res.send({message: " Se borró"}));
-const clients = require('./');
-
-
 module.exports = router;
